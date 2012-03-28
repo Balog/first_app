6eb346add880e82f34a87670949cbc784b0d347d
Alex Goncharov <balogi@yandex.ru>
2012-03-29 00:51:28 +0200

    First
    Signed-off-by: Alex Goncharov <balogi@yandex.ru>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '60975e3c90769142e4c660e7514fbc2e530d56e816f347071de42801880b682bb7c9cb6e4a848ddd3c0e093b7891a302a938cd41eea6b23e80dda4fe103969aa'
